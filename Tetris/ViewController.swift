361dfb69ddd9ee7ea047c12b15effca24ec3063d
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2023-06-29 21:24:25 +0900

    Initial Commit

---
+++ Tetris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tetris
+//
+//  Created by 五十嵐諒 on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
